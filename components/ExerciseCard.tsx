3d5485fa882141ddf1cba783a9ad35c02682d763
pauledwardmitchell <pauledwardmitchell@gmail.com>
2024-02-23 09:51:02 +0100

    Add cldvideoplayer and some props on video element in exercise card

--- components/ExerciseCard.tsx
+++ components/ExerciseCard.tsx
@@ -1,5 +1,3 @@
-"use client"
-
 import { getCldVideoUrl } from 'next-cloudinary'
 
 const ExerciseCard = ( {exercise} ) => {
@@ -12,7 +10,7 @@ const ExerciseCard = ( {exercise} ) => {
 	return (
 		<div className="w-full h-full">
 			<div>{exercise.name}</div>
-			<video width="800" height="600" controls >
+			<video width="800" height="600" controls playsInline preload="none">
 				<source src={url}/>
 			</video>
 		</div>
